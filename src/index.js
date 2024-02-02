7d03228c0457c049499b4ce6eb382f327c715971
NCatalin93 <catalin.nedelcu93@gmail.com>
2024-02-02 21:34:12 +0200

    add key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
 
-axios.defaults.headers.common['x-api-key'] = 'cheia ta';
+axios.defaults.headers.common['x-api-key'] =
+  'live_whBaHJ9RyGwNS5wD2gTvysRMYtNdsRZhnSbjjtJu3Bcg4CRn8x4OMdQBxGnkstVo';
 
 new SlimSelect({
   select: '#selectElement',
